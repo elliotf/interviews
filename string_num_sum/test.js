af70d47152b769e6a819d3626b72cac7becaab17
Elliot Foster <efoster@firetaco.com>
2013-09-12 18:21:40 -0700

    Failure 4: expected 0 to be 123

--- string_num_sum/test.js
+++ string_num_sum/test.js
@@ -6,4 +6,8 @@ describe("stringNumSum", function() {
   it("returns the result as a number", function() {
     expect(numSum('0')).to.equal(0);
   });
+
+  it("treats contiguous digits as one number", function() {
+    expect(numSum('123')).to.equal(123);
+  });
 });
