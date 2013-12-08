f9c6dc576ad4797be38f67251932069f2c335e5e
Elliot Foster <efoster@firetaco.com>
2013-12-07 18:11:23 -0800

    Sweeper: square model

--- minesweeper/main.js
+++ minesweeper/main.js
@@ -5,4 +5,36 @@ var fs = require('fs')
 function Game() {
 }
 
+function Square(isBomb) {
+  this._neighbors = [];
+  this.shown = false;
+  this._isBomb = !!isBomb;
+  this._numBombNeighbors = 0;
+}
+
+Square.prototype.addNeighbor = function(square) {
+  this._neighbors.push(square);
+
+  if (square.hasBomb()) this._numBombNeighbors++;
+}
+
+Square.prototype.reveal = function() {
+  this.shown = true;
+
+  if (!this._numBombNeighbors) {
+    this._neighbors.forEach(function(n){ n.reveal() });
+  }
+
+  return this._numBombNeighbors;
+}
+
+Square.prototype.hasBomb = function() {
+  return this._isBomb;
+}
+
+Square.prototype.bombNeighbors = function() {
+  return this._numBombNeighbors;
+}
+
 exports.Game = Game;
+exports.Square = Square;
--- minesweeper/test.js
+++ minesweeper/test.js
@@ -12,9 +12,165 @@ describe.only("Sweeper", function() {
     this.game = new sweeper.Game();
   });
 
-  describe("Game", function() {
+  describe("Game model", function() {
     it("can be instantiated", function() {
-      expect(this.game).to.be.ok;
+      expect(this.game).to.be.instanceof(sweeper.Game);
+    });
+  });
+
+  describe("Square model", function() {
+    beforeEach(function() {
+      this.square = new sweeper.Square();
+    });
+
+    it("can be instantiated", function() {
+      expect(this.square).to.be.instanceof(sweeper.Square);
+    });
+
+    describe("#hasBomb", function() {
+      describe("when the square does not have a bomb", function() {
+        it("returns false", function() {
+          expect(this.square.hasBomb()).to.be.false;
+        });
+      });
+
+      describe("when the square has a bomb", function() {
+        beforeEach(function() {
+          this.square = new sweeper.Square(true);
+        });
+
+        it("returns true", function() {
+          expect(this.square.hasBomb()).to.be.true;
+        });
+      });
+    });
+
+    describe("#addNeighbor", function() {
+      it("adds a neighbor to the given Square", function() {
+        expect(this.square._neighbors).to.have.length(0);
+
+        var neighbor = new sweeper.Square();
+        this.square.addNeighbor(neighbor);
+
+        expect(this.square._neighbors).to.have.length(1);
+        expect(this.square._neighbors[0]).to.equal(neighbor);
+      });
+
+      describe("when the neighbor has a bomb", function() {
+        it("increments the bombNeighbors counter", function() {
+          expect(this.square.bombNeighbors()).to.equal(0);
+
+          this.square.addNeighbor(new sweeper.Square(true));
+
+          expect(this.square.bombNeighbors()).to.equal(1);
+        });
+      });
+    });
+
+    describe("#reveal", function() {
+      beforeEach(function() {
+        this.bombNeighbor = new sweeper.Square(true);
+        this.sinon.spy(this.bombNeighbor, 'reveal');
+
+        this.safeNeighbor = new sweeper.Square();
+        this.sinon.spy(this.safeNeighbor, 'reveal');
+
+        this.square.addNeighbor(this.safeNeighbor);
+      });
+
+      it("changes its shown state", function() {
+        expect(this.square.shown).to.be.false;
+
+        this.square.reveal();
+
+        expect(this.square.shown).to.be.true;
+      });
+
+      describe("when the square is a bomb", function() {
+        beforeEach(function() {
+          this.square = new sweeper.Square(true);
+
+          this.square.addNeighbor(this.safeNeighbor);
+          this.square.addNeighbor(this.bombNeighbor);
+        });
+
+        it("changes its state to shown", function() {
+          expect(this.square.shown).to.be.false;
+
+          this.square.reveal();
+
+          expect(this.square.shown).to.be.true;
+        });
+
+        it("does not call #reveal on its neighbors", function() {
+          expect(this.bombNeighbor.reveal).to.not.have.been.called;
+          expect(this.safeNeighbor.reveal).to.not.have.been.called;
+
+          this.square.reveal();
+
+          expect(this.bombNeighbor.reveal).to.not.have.been.called;
+          expect(this.safeNeighbor.reveal).to.not.have.been.called;
+        });
+
+        it("returns -1 to represent 'game over'", function() {
+        });
+      });
+
+      describe("when the square is not a bomb", function() {
+        describe("and has no bomb neighbors", function() {
+          it("changes its state to shown", function() {
+            expect(this.square.shown).to.be.false;
+
+            this.square.reveal();
+
+            expect(this.square.shown).to.be.true;
+          });
+
+          it("calls #reveal on its neighbors", function() {
+            expect(this.safeNeighbor.reveal).to.not.have.been.called;
+
+            this.square.reveal();
+
+            expect(this.safeNeighbor.reveal).to.have.been.called;
+          });
+
+          it("returns the number of bomb neighbors", function() {
+            expect(this.square.reveal()).to.equal(0);
+          });
+        });
+
+        describe("but has bomb neighbors", function() {
+          beforeEach(function() {
+            this.square.addNeighbor(this.bombNeighbor);
+          });
+
+          it("changes its state to shown", function() {
+            expect(this.square.shown).to.be.false;
+
+            this.square.reveal();
+
+            expect(this.square.shown).to.be.true;
+          });
+
+          it("does not call #reveal on its neighbors", function() {
+            expect(this.bombNeighbor.reveal).to.not.have.been.called;
+            expect(this.safeNeighbor.reveal).to.not.have.been.called;
+
+            this.square.reveal();
+
+            expect(this.bombNeighbor.reveal).to.not.have.been.called;
+            expect(this.safeNeighbor.reveal).to.not.have.been.called;
+          });
+
+          it("returns the number of bomb neighbors", function() {
+            expect(this.square.reveal()).to.equal(1);
+
+            this.square.addNeighbor(new sweeper.Square(true));
+
+            expect(this.square.reveal()).to.equal(2);
+          });
+        });
+      });
     });
   });
 });
